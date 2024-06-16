1c893809067f374ae36a69aeb5ae7c6bd1cf196d
rudrax14 <m.rudra2003@gmail.com>
2024-06-16 21:15:12 +0530

    populate user name from getAllJobsPostedByCompany

--- backend/controllers/companyControllers.js
+++ backend/controllers/companyControllers.js
@@ -1,78 +1,73 @@
-const Company = require('../models/Company');
-const AppError = require('../utils/AppError');
-const catchAsync = require('../utils/catchAsync');
-
+const Company = require("../models/Company");
+const AppError = require("../utils/AppError");
+const catchAsync = require("../utils/catchAsync");
 
 const filterCompanyObject = (userObj, attrs) => {
-
-    const filteredObj = Object.keys(userObj)
-        .filter(attr => !(attrs.includes(attr)))
-        .reduce((obj, key) => {
-            obj[key] = userObj[key];
-            return obj;
-        }, {});
-    return filteredObj;
-}
-
-
-
-
-
+  const filteredObj = Object.keys(userObj)
+    .filter((attr) => !attrs.includes(attr))
+    .reduce((obj, key) => {
+      obj[key] = userObj[key];
+      return obj;
+    }, {});
+  return filteredObj;
+};
 
 exports.companyProfile = catchAsync(async (req, res, next) => {
-    const companyProfile = await Company.findById(req.user.id);
-    // const CompanyProfile = {
-    //     Companyname:CompanyDetails.Companyname,
-    //     email:CompanyDetails.email,
-    //     fullName:CompanyDetails.fullName,
-    //     location:CompanyDetails.location&&CompanyDetails.location!==""?CompanyDetails.location:
-    // }
-    res.status(200).json({
-        status: 'Success',
-        profile: companyProfile
-    })
+  const companyProfile = await Company.findById(req.user.id);
+  // const CompanyProfile = {
+  //     Companyname:CompanyDetails.Companyname,
+  //     email:CompanyDetails.email,
+  //     fullName:CompanyDetails.fullName,
+  //     location:CompanyDetails.location&&CompanyDetails.location!==""?CompanyDetails.location:
+  // }
+  res.status(200).json({
+    status: "Success",
+    profile: companyProfile,
+  });
 });
 
 exports.updateCompanyProfile = catchAsync(async (req, res, next) => {
+  const companyObj = filterCompanyObject(req.body, [
+    "password",
+    "fullName",
+    "username",
+    "email",
+  ]);
+  const query = {
+    $set: {
+      updatedAt: Date.now(),
+      ...companyObj,
+    },
+  };
 
-
-    const companyObj = filterCompanyObject(req.body, ["password", "fullName", "username", "email"]);
-    const query = {
-        $set: {
-            updatedAt: Date.now(),
-            ...companyObj
-        }
-    };
-
-    await Company.findByIdAndUpdate(req.user.id, query);
-    res.status(200).json({
-        status: 'Success',
-        message: "Company profile successfully updated"
-    });
+  await Company.findByIdAndUpdate(req.user.id, query);
+  res.status(200).json({
+    status: "Success",
+    message: "Company profile successfully updated",
+  });
 });
 
-
-
 exports.getAllJobsPostedByCompany = catchAsync(async (req, res, next) => {
-    const companyId = req.user.id;
+  const companyId = req.user.id;
 
+  // Find the company by ID and populate the jobs array
+  const company = await Company.findById(companyId).populate({
+    path: "jobs",
+    populate: {
+      path: "appliedUsers.userId",
+      select: "name",
+    },
+  });
 
-    // Find the company by ID
-    const company = await Company.findById(companyId);
+  if (!company) {
+    return next(new AppError("Company not found", 404));
+  }
 
-    if (!company) {
-        next(new AppError('Company not found', 404));
-    }
+  // Extract the jobs from the populated company object
+  const jobsPostedByCompany = company.jobs;
 
-    // Populate the jobs array of the company
-    await company.populate('jobs');
-
-    // Extract the jobs from the populated company object
-    const jobsPostedByCompany = company.jobs;
-
-    res.status(200).json({
-        status: 'Success',
-        jobs: jobsPostedByCompany,
-    });
-
+  res.status(200).json({
+    status: "Success",
+    jobs: jobsPostedByCompany,
+  });
 });
